1ee9a11c8993d142af01fed16aa9bcf414a4f034
carinalarriva <carinalarriva@gmail.com>
2026-04-13 08:29:56 -0500

    first commit

---
+++ ejercicio1 heroe.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+
+using namespace std;
+
+void mostrarEstado(string nombre, int hp, int maxHP) {
+    cout << nombre << " - " << "HP: " << hp << "/" << maxHP << endl;
+}
+
+void recibirDanio(int& hp, int danio) {
+    hp -= danio;
+    if (hp < 0) {
+        hp = 0;
+    }
+}
+
+void curar(int& hp, int cantidad, int maxHP) {
+    hp += cantidad;
+    if (hp > maxHP) {
+        hp = maxHP;
+    }
+}
+
+int main () {
+    string nombre = "Arthas";
+    int hp = 100;
+    int maxHP = 100;
+
+    cout << "=== Estado del Heroe ===" << endl;
+    mostrarEstado(nombre, hp, maxHP);
+
+    cout << nombre << " recibe 30 de daño" << endl;
+    recibirDanio(hp, 30);
+    mostrarEstado(nombre, hp, maxHP);
+
+    cout << nombre << " recibe 50 de daño" << endl;
+    recibirDanio(hp, 50);
+    mostrarEstado(nombre, hp, maxHP);
+
+    cout << nombre << " se cura 40 puntos" << endl;
+    curar(hp, 40, maxHP);
+    mostrarEstado(nombre, hp, maxHP);
+
+    cout << nombre << " recibe 80 de daño" << endl;
+    recibirDanio(hp, 80);
+    mostrarEstado(nombre, hp, maxHP);
+}
---
+++ parte1.md
@@ -0,0 +1,13 @@
+Parte 1
+
+1. b) 50
+2. a) 100
+3. a) 100
+4. b) El valor almacenado en valor
+    (25)
+5. b) Con referencia: funcion(variable). Con puntero: funcion(&variable) y dentro se usa *parametro
+6.  b) 20 10
+
+
+
+
